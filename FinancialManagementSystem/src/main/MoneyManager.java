512a74a9615f711128d0a5cac53502e86286894f
SemwKen <1085173347@qq.com>
2025-05-23 20:33:46 +0800

    Add files via upload

---
+++ FinancialManagementSystem/src/main/MoneyManager.java
@@ -0,0 +1,10 @@
+package main;
+import javax.swing.*;
+//程序的开始
+public class MoneyManager {
+	public static void main(String[] args) {
+		 LoginFrame lf=new LoginFrame();
+		 lf.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+}
+ 
